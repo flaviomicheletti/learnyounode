5819405fcdf3bd22397d1009d8a50bad88baa9ac
Flávio Micheletti <flaviomicheletti@hotmail.com>
2019-12-21 15:00:59 -0300

    02

---
+++ 02-baby-steps.js
@@ -0,0 +1,15 @@
+//
+// http://www.devfuria.com.br/node.js/learnyounode-02-baby-steps/
+//
+
+
+'use strict'
+    
+let result = 0
+
+for (let i = 2; i < process.argv.length; i++) {
+  result += Number(process.argv[i])
+}
+
+console.log(result)
+
