af334ac5b95df24c9713acc5e06be81a0c86a569
Flávio Micheletti <flaviomicheletti@hotmail.com>
2019-12-25 19:57:41 -0300

    05-filtered-ls

---
+++ 05-filtered-ls.js
@@ -0,0 +1,19 @@
+//
+// http://www.devfuria.com.br/node.js/learnyounode-05-filtered-ls/
+//
+'use strict'
+
+var fs   = require('fs')
+var path = require('path')
+
+var folder = process.argv[2]
+var ext    = '.' + process.argv[3]
+
+fs.readdir(folder, function (err, list) {
+  if (err) return console.error(err)
+  list.forEach(function(file) {
+      if (path.extname(file) === ext) {
+          console.log(file)
+      }
+  })
+})
